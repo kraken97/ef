7957fc6dc3d5f676ca17fee5a5cfc760d664b5cc
Dima Zherebko <kraken@live.ru>
2016-07-28 01:55:56 +0300

    initial

---
+++ Migrations/20160727223600_initial.Designer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using DatabaseApplication;
+
+namespace ef.Migrations
+{
+    [DbContext(typeof(SqliteDbContext))]
+    [Migration("20160727223600_initial")]
+    partial class initial
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0-rtm-21431");
+
+            modelBuilder.Entity("DatabaseApplication.Blog", b =>
+                {
+                    b.Property<int>("BlogId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Url")
+                        .HasAnnotation("MaxLength", 500);
+
+                    b.HasKey("BlogId");
+
+                    b.ToTable("Blogs");
+                });
+
+            modelBuilder.Entity("DatabaseApplication.Post", b =>
+                {
+                    b.Property<int>("PostId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int?>("BlogId");
+
+                    b.Property<string>("Content")
+                        .HasAnnotation("MaxLength", 2500);
+
+                    b.Property<string>("Title")
+                        .HasAnnotation("MaxLength", 200);
+
+                    b.HasKey("PostId");
+
+                    b.HasIndex("BlogId");
+
+                    b.ToTable("Posts");
+                });
+
+            modelBuilder.Entity("DatabaseApplication.Post", b =>
+                {
+                    b.HasOne("DatabaseApplication.Blog", "Blog")
+                        .WithMany("Posts")
+                        .HasForeignKey("BlogId");
+                });
+        }
+    }
+}
